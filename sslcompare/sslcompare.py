2c32f70807d06a5a331b826831edac35606461c3
LivinParadoX <alexandre.janvrin@reseau.eseo.fr>
2021-07-11 12:31:53 +0200

    new file:   .gitignore         deleted:    poetry.lock         deleted:    pyproject.toml         deleted:    sslcompare/__init__.py         new file:   sslcompare/anssi.json         deleted:    sslcompare/anssi.yaml         modified:   sslcompare/sslcompare.py         deleted:    sslcompare/testssl.sh/bin/openssl.Linux.i686         new file:   sslcompare/testssl.sh/etc/Apple.pem         new file:   sslcompare/testssl.sh/etc/Java.pem         new file:   sslcompare/testssl.sh/etc/Linux.pem         new file:   sslcompare/testssl.sh/etc/Microsoft.pem         new file:   sslcompare/testssl.sh/etc/Mozilla.pem         new file:   sslcompare/testssl.sh/etc/README.md         new file:   sslcompare/testssl.sh/etc/ca_hashes.txt         new file:   sslcompare/testssl.sh/etc/client-simulation.txt         new file:   sslcompare/testssl.sh/etc/client-simulation.wiresharked.md         new file:   sslcompare/testssl.sh/etc/client-simulation.wiresharked.txt         new file:   sslcompare/testssl.sh/etc/common-primes.txt         new file:   sslcompare/testssl.sh/etc/curves-mapping.txt         new file:   sslcompare/testssl.sh/etc/curves.txt         deleted:    sslcompare_standalone.py

--- sslcompare/sslcompare.py
+++ sslcompare/sslcompare.py
@@ -9,26 +9,24 @@ Authors :
   - Adrian Kalmar
   - Alexandre Janvrin.
 """
+import json
 import re
-import shlex
 import subprocess
-from functools import partial
-from importlib import resources
-
-import click
-import yaml
-
-with resources.path("sslcompare", "anssi.yaml") as default_baseline_path:
-    DEFAULT_BASELINE_PATH = default_baseline_path
+from sys import argv
 
 
-@click.command()
-@click.argument("url")
-@click.option("-v", "--verbose", is_flag=True)
-@click.option(
-    "--baseline", default=DEFAULT_BASELINE_PATH, help="baseline file (yaml)."
-)
-def main(url, baseline, verbose=False):
+def style(text, fg):
+    colors = {"red": 31, "yellow": 33, "green": 32}
+    return "".join(
+        [
+            f"\x1b[{colors[fg]};1m",
+            text,
+            "\x1b[0m",
+        ]
+    )
+
+
+def main(url, verbose=False):
     """Scan URL's cipher suites and compares it to the BASELINE.
 
     Cipher suites are retrieved with the testssl.sh shell script
@@ -38,92 +36,91 @@ def main(url, baseline, verbose=False):
         sslcompare https://mytargetsite.com -b /path/to/baseline.yaml
         sslcompare 127.0.0.1:8080
     """
-    strip_ansi = partial(re.compile(r"\x1b\[\d*m").sub, "")
+    strip_ansi = re.compile(r"\x1b\[\d*m").sub
 
-    with open(baseline) as baseline_path:
-        baseline_suites = yaml.safe_load(baseline_path)
+    with open("anssi.json") as baseline_path:
+        baseline_suites = json.load(baseline_path)
 
-    with resources.as_file(resources.files("sslcompare")) as sslcompare_path:
-        with subprocess.Popen(
-            shlex.split(
-                f"{sslcompare_path / 'testssl.sh/testssl.sh'} -E -U {url}"
-            ),
-            bufsize=1,
-            universal_newlines=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        ) as testssl:
+    with subprocess.Popen(
+        ["./testssl.sh/testssl.sh", "-E", "-U", url],
+        bufsize=1,
+        universal_newlines=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    ) as testssl:
 
-            current_protocol = None
-            interesting_lines = False
+        current_protocol = None
+        interesting_lines = False
 
-            for line in testssl.stdout:
-                line = line.rstrip()
+        for line in testssl.stdout:
+            line = line.rstrip()
 
-                if "Start" in line:
-                    click.echo(line)
+            if "Start" in line:
+                print(line)
 
-                elif "Done" in line:
-                    click.echo(line)
-                    break
+            elif "Done" in line:
+                print(line)
+                break
 
-                elif "Testing" in line:
-                    if not (verbose or interesting_lines):
-                        click.echo("")
+            elif "Testing" in line:
+                if not (verbose or interesting_lines):
+                    print("")
 
-                    click.echo(line)
-                    interesting_lines = True
+                print(line)
+                interesting_lines = True
 
-                elif "Cipher Suite Name (IANA/RFC)" in line:
-                    click.echo(
-                        f" {'Cipher Suite Name (IANA/RFC)':41} Evaluation"
-                    )
+            elif "Cipher Suite Name (IANA/RFC)" in line:
+                print(f" {'Cipher Suite Name (IANA/RFC)':41} Evaluation")
 
-                elif strip_ansi(line) in [
-                    "SSLv2",
-                    "SSLv3",
-                    "TLS 1",
-                    "TLS 1.1",
-                    "TLS 1.2",
-                    "TLS 1.3",
-                ]:
-                    current_protocol = strip_ansi(line)
-                    click.echo(line)
-                    continue
+            elif strip_ansi("", line) in [
+                "SSLv2",
+                "SSLv3",
+                "TLS 1",
+                "TLS 1.1",
+                "TLS 1.2",
+                "TLS 1.3",
+            ]:
+                current_protocol = strip_ansi("", line)
+                print(line)
+                continue
 
-                elif current_protocol is not None and line != "":
-                    if line == " -":
-                        if current_protocol in [
-                            "SSLv2",
-                            "SSLv3",
-                            "TLS 1",
-                            "TLS 1.1",
-                        ]:
-                            click.echo(
-                                f" {'NOT OFFERED':<41} "
-                                + click.style("[RECOMMENDED]", fg="green")
-                            )
-                        else:
-                            click.echo(f" {'NOT OFFERED':<41}")
-
+            elif current_protocol is not None and line != "":
+                if line == " -":
+                    if current_protocol in [
+                        "SSLv2",
+                        "SSLv3",
+                        "TLS 1",
+                        "TLS 1.1",
+                    ]:
+                        print(
+                            f" {'NOT OFFERED':<41} "
+                            + style("[RECOMMENDED]", fg="green")
+                        )
                     else:
-                        cipher_suite = line.split()[-1]
+                        print(f" {'NOT OFFERED':<41}")
 
-                        try:
-                            click.echo(
-                                f" {cipher_suite:<41} "
-                                + click.style(
-                                    **baseline_suites[current_protocol][
-                                        cipher_suite
-                                    ]
-                                )
+                else:
+                    cipher_suite = line.split()[-1]
+
+                    try:
+                        print(
+                            f" {cipher_suite:<41} "
+                            + style(
+                                **baseline_suites[current_protocol][
+                                    cipher_suite
+                                ]
                             )
+                        )
 
-                        except KeyError:
-                            click.echo(
-                                f" {cipher_suite: <41} "
-                                + click.style("[DEPRECATED ]", fg="red")
-                            )
+                    except KeyError:
+                        print(
+                            f" {cipher_suite: <41} "
+                            + style("[DEPRECATED ]", fg="red")
+                        )
 
-                elif interesting_lines or verbose:
-                    click.echo(line)
+            elif interesting_lines or verbose:
+                print(line)
+
+
+if __name__ == "__main__":
+    main(argv[1])
